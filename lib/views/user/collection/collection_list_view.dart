c7c7705e65e6da05aa89c71f8ae00dea0339b81c
Sinhularity <zS22017021@estudiantes.uv.mx>
2025-04-30 17:10:27 -0600

    collection-details-view: details fixed

--- lib/views/user/collection/collection_list_view.dart
+++ lib/views/user/collection/collection_list_view.dart
@@ -1,6 +1,9 @@
 import 'package:acopiatech/constants/colors_palette.dart';
+import 'package:acopiatech/services/cloud/collections/bloc/collection_bloc.dart';
 import 'package:acopiatech/services/cloud/collections/collection.dart';
+import 'package:acopiatech/views/user/collection/user_collection_details_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 // typedef Callback = void Function(Collection collections);
 class CollectionListView extends StatelessWidget {
@@ -27,72 +30,88 @@ class CollectionListView extends StatelessWidget {
     if (collections.isEmpty) {
       return const Text('No existen recolecciones');
     }
-    return Card(
-      child: Stack(
-        children: [
-          Positioned.fill(
-            bottom: 1,
-            left: 1,
-            right: 1,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white60,
-              ),
-            ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder:
+                (_) => BlocProvider.value(
+                  value: BlocProvider.of<CollectionBloc>(context),
+                  child: UserCollectionDetailsView(
+                    collection: collections.first,
+                  ),
+                ),
           ),
-          Material(
-            color: Colors.transparent,
-            child: ListTile(
-              title: Text(
-                'Recolección a domicilio',
-                style: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w800,
-                  color: Colors.black,
+        );
+      },
+      child: Card(
+        child: Stack(
+          children: [
+            Positioned.fill(
+              bottom: 1,
+              left: 1,
+              right: 1,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white60,
                 ),
               ),
-              subtitle: Row(
-                spacing: 30,
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Entregar en:',
-                          style: const TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.black,
-                          ),
-                        ),
-                        Text(
-                          '${getAddress()}',
-                          overflow: TextOverflow.clip,
-                          style: const TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w400,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Text(
-                    '${getStatus()}',
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.black,
-                    ),
-                  ),
-                ],
-              ),
-              trailing: const Icon(Icons.circle, color: Colors.amber),
             ),
-          ),
-        ],
+            Material(
+              color: Colors.transparent,
+              child: ListTile(
+                title: Text(
+                  'Recolección a domicilio',
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.black,
+                  ),
+                ),
+                subtitle: Row(
+                  spacing: 30,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Entregar en:',
+                            style: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Text(
+                            '${getAddress()}',
+                            overflow: TextOverflow.clip,
+                            style: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Text(
+                      '${getStatus()}',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+                trailing: const Icon(Icons.circle, color: Colors.amber),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
