a4394dd05c07c08c7b57dfa6374ad6b88de088b6
Mundus <zS22017046@estudiantes.uv.mx>
2025-05-13 15:12:34 -0600

    main: added dropoff markers to user view

--- lib/services/maps/geocoding_service.dart
+++ lib/services/maps/geocoding_service.dart
@@ -1,17 +1,48 @@
+import 'dart:developer' show log;
+
+import 'package:acopiatech/services/cloud/address/address_storage.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 Future<LatLng> getCoordsFromAddress({required String address}) async {
-    try {
-      List<Location> locations = await locationFromAddress(address);
+  try {
+    List<Location> locations = await locationFromAddress(address);
 
-      if (locations.isNotEmpty) {
-        final location = locations.first;
-        return LatLng(location.latitude, location.longitude);
-      } else {
-        return const LatLng(18.14431461778299, -94.47604801699983);
-      }
-    } on Exception {
-      rethrow;
+    if (locations.isNotEmpty) {
+      final location = locations.first;
+      return LatLng(location.latitude, location.longitude);
+    } else {
+      log("Could not get location from address");
+      return const LatLng(18.14431461778299, -94.47604801699983);
     }
+  } on Exception {
+    rethrow;
   }
+}
+
+Stream<Set<Marker>> getDropOffs() async* {
+  final AddressStorage addressService = AddressStorage();
+  log('Getting dropoffs');
+  final Set<Marker> dropoffMarkers = {};
+
+  await for (final dropoffs in addressService.allDropOffs()) {
+    log('Dropoff list: ${dropoffs.isEmpty.toString()}');
+
+    final markerFuture =
+        dropoffs.map((dropoff) async {
+          final addressLocation = await getCoordsFromAddress(
+            address: dropoff.toString(),
+          );
+          log('Dropoff coords: ${addressLocation.toString()}');
+
+          return Marker(
+            markerId: MarkerId(dropoff.toString()),
+            position: addressLocation,
+            infoWindow: InfoWindow(title: dropoff.toString()),
+          );
+        }).toList();
+
+    final markers = await Future.wait(markerFuture);
+    yield markers.toSet();
+  }
+}
--- lib/widgets/drop_off_map.dart
+++ lib/widgets/drop_off_map.dart
@@ -1,4 +1,9 @@
+import 'dart:developer' show log;
+
 import 'package:acopiatech/constants/map_style.dart';
+import 'package:acopiatech/services/cloud/address/address.dart';
+import 'package:acopiatech/services/cloud/address/address_storage.dart';
+import 'package:acopiatech/services/maps/geocoding_service.dart';
 import 'package:acopiatech/services/maps/geolocator_service.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -13,10 +18,14 @@ class DropOffMap extends StatefulWidget {
 class _DropOffMapState extends State<DropOffMap> {
   late final GeolocatorService _geolocatorService;
   late GoogleMapController _googleMapController;
+  late final AddressStorage _addressStorage;
+  late final Set<Marker> _dropoff_markers;
 
   @override
   void initState() {
     _geolocatorService = GeolocatorService();
+    _addressStorage = AddressStorage();
+    _dropoff_markers = {};
     super.initState();
   }
 
@@ -27,34 +36,41 @@ class _DropOffMapState extends State<DropOffMap> {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: _geolocatorService.getCurrentLocationUpdates(),
+      stream: getDropOffs(),
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
-          case ConnectionState.waiting:
           case ConnectionState.active:
             if (snapshot.hasData) {
-              final location = snapshot.data!;
-              return GoogleMap(
-                myLocationEnabled: true,
-                initialCameraPosition: CameraPosition(
-                  target: location,
-                  zoom: 13.66,
-                ),
-                onMapCreated: (controller) => _onMapCreated(controller),
-                style: noPoiMapStyle,
-                markers: {
-                  Marker(
-                    markerId: MarkerId('yo'),
-                    position: location,
-                    infoWindow: InfoWindow(
-                      title: "Tu ubicación",
-                      snippet: "Yo merengues",
-                    ),
-                  ),
+              final dropoffMarkers = snapshot.data as Set<Marker>;
+              return StreamBuilder(
+                stream: _geolocatorService.getCurrentLocationUpdates(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                    case ConnectionState.active:
+                      if (snapshot.hasData) {
+                        final location = snapshot.data!;
+                        return GoogleMap(
+                          myLocationEnabled: true,
+                          initialCameraPosition: CameraPosition(
+                            target: location,
+                            zoom: 13.66,
+                          ),
+                          onMapCreated:
+                              (controller) => _onMapCreated(controller),
+                          style: noPoiMapStyle,
+                          markers: dropoffMarkers,
+                        );
+                      } else {
+                        return const Center(child: Text(''));
+                      }
+                    default:
+                      return const Center(child: CircularProgressIndicator());
+                  }
                 },
               );
             } else {
-              return const Center(child: Text(''));
+              return const Center(child: CircularProgressIndicator());
             }
           default:
             return const Center(child: CircularProgressIndicator());
